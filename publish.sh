002d48248519755b351c78c603391a9b8eacf2c3
Elijah Riggs <eli@rje.li>
2024-07-03 20:56:10 -0400

    publish.sh

--- publish.sh
+++ publish.sh
@@ -1,1 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -euo pipefail
+
+git branch -f publish master
+git checkout publish
+./build.py
+git add -f docs
+git commit -am "publish"
+git push
+git checkout master
