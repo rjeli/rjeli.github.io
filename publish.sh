f4ec34cbe1976112e2bf2334f96b8d93a6357a93
Elijah Riggs <eli@rje.li>
2024-07-03 20:59:40 -0400

    sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ ! -z "$(git status --porcelain)" ]; then
+    echo uncommitted changes
+    exit 1
+fi
+
 git branch -f publish master
 git checkout publish
 ./build.py
+echo "rje.li" >docs/CNAME
 git add -f docs
 git commit -am "publish"
 git push
